6e898b774211e33e5cefc666b200b4c00c7eeb52
puelloguillermo13-cell <puelloguillermo13@gmail.com>
2025-08-11 13:43:25 -0500

    Fix Tailwind PostCSS config for Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
